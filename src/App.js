2ac85315a6d6bb44a4d9750ec487b373066a1b04
Ankit Singh <2501.ankit@gmail.com>
2020-07-23 11:57:09 +0530

    add typescript

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header'
 
 function App() {
   return (
     <div className="App">
-      <h1>Tekonika website</h1>
+      <Header />
+      <Home />
     </div>
   );
 }
---
+++ src/Components/Header/Header.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Header = () : JSX.Element => (
+    <h1>Header</h1>
+)
+
+export default Header;
