8e1cca8e19ee041dcef17b7bf74cc0340194fd67
Nikkhil Butola <nikhil.b@lucideustech.com>
2020-07-30 18:45:16 +0530

    UISection

--- src/Components/UI_upperSection/CardUISection.tsx
+++ src/Components/UI_upperSection/CardUISection.tsx
@@ -1,22 +1,62 @@
 import React from "react";
 import { Row, Col, Container } from "../Common/Grid";
+import Button from "@material-ui/core/Button";
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 
 import "./UIsection.css";
 
 const CardSection = (): JSX.Element => {
     
-   return (
-       <Container style={{width:'100%'}}>
-           <Row justify="space-evenly">
-               <Col xs={12} sm={12} md={5} lg={5}>
-                   <div style={{height: '300px', background: 'blue'}}></div>
-               </Col>
-
-               <Col xs={12} sm={12} md={5} lg={5}>
-                    <div style={{height: '300px', background: 'yellow'}}></div>
-               </Col>
-           </Row>
-       </Container>
+    return (
+        <div style={{ width: "200", height: "200%", paddingTop: "120px", 
+        paddingBottom: "100px", backfaceVisibility: "hidden", boxSizing: "border-box", 
+        backgroundColor: "#f2f2f2" }}>
+            <Row >
+                <Col xs={12} sm={12} md={5} lg={6} style= {{ paddingLeft: "80px", paddingRight: "15px" }} >
+                    <div style={{height: '400px', width: "100%", background: '#0085f2'}}>
+                        <div style={{ paddingLeft: "70px", paddingRight: "70px", paddingTop: "110px" }}>
+                            <span className="card-heading">
+                            You are in the right place
+                            </span>
+                        </div>
+                        <p className="sub-heading-text" style={{ paddingLeft: "70px", paddingRight: "80px" }} >
+                            <span style={{ color: "#ffffff" }}>
+                            We are your partner in designs and we collaborate <br />exquisitely great with businesses like yours. 
+                            We help<br /> businesses gain confidence in their product and<br /> position them as brands that clients can trust.
+                            </span>
+                            <div style={{ paddingTop: "34px" }} >
+                                <Button className="learn-more" variant="contained" >LEARN MORE<ArrowForwardIosIcon /> </Button>
+                            </div>
+                        </p>
+                        
+                    </div>
+                </Col>
+                <Col xs={12} sm={12} md={5} lg={6} style= {{ paddingLeft: "15px", paddingRight: "80px" }} >
+                    <div style={{height: '400px', width: "100%", background: '#ffffff'}}>
+                        <div style={{ paddingLeft: "70px", paddingRight: "70px", paddingTop: "70px" }}>
+                            <span className="card-heading" style={{ color: "#0085f2" }}>
+                            Great design offerings
+                            </span>
+                        </div>
+                        <br />
+                        <p className="sub-heading-text" style={{ paddingLeft: "70px", paddingRight: "80px" }}>
+                            <span style={{ color: "#0085f2" }}>
+                            We take problems and build solutions around them. 
+                            We create concepts and implement designs that expresses 
+                            your brand values. We integrate the essence of great human 
+                            experience into designs from beautiful interfaces to innovative 
+                            user interactions.
+                            </span>
+                            <div style={{ paddingTop: "37px" }} >
+                                <Button className="learn-more" variant="contained" >LEARN MORE<ArrowForwardIosIcon /> </Button>
+                            </div>
+                        </p>
+                        
+                    </div>
+                </Col>
+            </Row>
+        
+        </div>
    )
 };
 
--- src/Components/UI_upperSection/UIUpperSection.tsx
+++ src/Components/UI_upperSection/UIUpperSection.tsx
@@ -20,7 +20,7 @@ import "./UIsection.css";
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
-        maxWidth: 345,
+        minWidth: 345,
         backgroundColor: "#0085f2",
       },
 
@@ -42,7 +42,7 @@ const UiDesign = (): JSX.Element => {
     
     return (
         <>
-            <div style={{width: "100%", minHeight: '700px'}}  className=" back-img">
+            <div style={{width: "100%", minHeight: '600px'}}  className=" back-img">
                 <Container style={{height: '100%'}}>
                     <Row justify="flex-start" alignItems="center">
                         <Col xs={12} sm={12} md={6} lg={4}>
@@ -56,80 +56,76 @@ const UiDesign = (): JSX.Element => {
                 </Container>
             </div>
             <CardSection />
-            <div className="icon-section-container">
-                <Container>
-                    <Row style={{ paddingTop: '30px' }}>
-                        <Col xs={11} sm={11} md={4} lg={4} style={{ paddingLeft: '50px'}}>
-                            <span className="UIheading-text" style={{ paddingTop: "29px" }}>An exceptionally <span className="weight-text">UI UX </span>design unique experience tailored to you</span>
-                            <div style={{ width:'100%', paddingTop: "30px", boxSizing: "border-box", height: "30px" }} >
-                                <Row >
-                                  <Col xs={12} sm={12} md={6} lg={6}>
-                                    <img src={MidSplash} width="400vw" height="269vh" alt="" />
-                                  </Col>
-                                </Row>
-                                <div style={{ paddingTop: "30px", boxSizing: "border-box", height: "30px", paddingLeft: "3px" }} >
-                                    <Button className="ui-learn-more"> Get Started  </Button>
-                                </div>
-                            </div>
-                            
+            <div className="container_1">
+            <Container style={{ paddingTop: "30px", paddingBottom: "40px" }}> 
+            <Row style={{ paddingTop: '30px' }}>
+                <Col xs={11} sm={11} md={4} lg={4} style={{ paddingLeft: '50px'}}>
+                    <span className="UIheading-text" style={{ paddingTop: "29px" }}>An exceptionally <span className="weight-text">UI UX </span>design unique experience tailored to you</span>
+                    <div style={{ width:'100%', paddingTop: "30px", boxSizing: "border-box", height: "30px" }} >
+                        <Row >
+                          <Col xs={12} sm={12} md={6} lg={6}>
+                            <img src={MidSplash} width="400vw" height="269vh" alt="" />
+                          </Col>
+                        </Row>
+                        <div style={{ paddingTop: "30px", boxSizing: "border-box", height: "30px", paddingLeft: "3px" }} >
+                            <Button className="ui-learn-more"> Get Started  </Button>
+                        </div>
+                    </div>
+                    
+                </Col>
+                <Col xs={12} sm={12} md={2} lg={2}></Col>
+                <Col xs={11} sm={11} md={5} lg={5}>
+                    <Row justify="space-between" style={{ paddingLeft: "-50px" }} >
+                        <Col xs={11} sm={11} md={5} lg={5} className="col-background-hover" >
+                          <div className="icons-blue"><PoolIcon /></div>
+                          <span style={{ fontSize: "28.9px", fontFamily: "Raleway", fontWeight: 400, lineHeight: "32px", opacity: "1" }}>Insight Driven</span>
+                          <p style={{ fontSize: "16px", lineHeight: "29px", opacity: "0.75", color: "#444", fontStyle: "inherit", textAlign: "left" }}>
+                            We leverage on the power of information and research to find out specific solutions to design problems</p>
                         </Col>
-                        <Col xs={12} sm={12} md={2} lg={2}></Col>
-                        <Col xs={11} sm={11} md={5} lg={5}>
-                            <Row justify="space-between" style={{ paddingLeft: "-50" }} >
-                                <Col xs={11} sm={11} md={5} lg={5}>
-                                  <div className="icons-blue"><PoolIcon /></div>
-                                  <span style={{ fontSize: "28.9px", fontFamily: "Raleway", fontWeight: 400, lineHeight: "32px", opacity: "1" }}>Insight Driven</span>
-                                  <p style={{ fontSize: "16px", lineHeight: "29px", opacity: "0.75", paddingBottom: "0px", color: "#444", fontStyle: "inherit", textAlign: "left" }}>
-                                    We leverage on the power of information and research to find out specific solutions to design problems</p>
-                                </Col>
-                                <Col xs={11} sm={11} md={5} lg={5}>
-                                  <div className="icons-yellow"><EmojiNatureIcon /></div>
-                                  <span style={{ fontSize: "28.9px", fontFamily: "Raleway", fontWeight: 400, lineHeight: "32px", opacity: "1" }}>Elegant Design</span>
-                                  <p style={{ fontSize: "16px", lineHeight: "29px", opacity: "0.75", paddingBottom: "0px", color: "#444", fontStyle: "inherit", textAlign: "left" }}>
-                                    We understand the impact of beautiful designs and stunning works of art. We build with style using tools like figma</p>
-                                </Col>
-                            </Row>
-                            <Row justify="space-between" style={{ paddingLeft: "-50" }} >
-                                <Col xs={11} sm={11} md={5} lg={5}>
-                                  <div className="icons-yellow"><SportsKabaddiIcon /></div>
-                                  <span style={{ fontSize: "28.9px", fontFamily: "Raleway", fontWeight: 400, lineHeight: "32px", opacity: "1" }}>User Centric</span>
-                                  <p style={{ fontSize: "16px", lineHeight: "29px", opacity: "0.75", paddingBottom: "0px", color: "#444", fontStyle: "inherit", textAlign: "left" }}>
-                                    At the center of our designs is the belief that the user is the definer of purpose. We thus create designs that improve human experience. </p>
-                                </Col>
-                                <Col xs={11} sm={11} md={5} lg={5}>
-                                  <div className="icons-blue"><GestureIcon /></div>
-                                  <span style={{ fontSize: "28.9px", fontFamily: "Raleway", fontWeight: 400, lineHeight: "32px", opacity: "1" }}>Quality Tested</span>
-                                  <p style={{ fontSize: "16px", lineHeight: "29px", opacity: "0.75", paddingBottom: "0px", color: "#444", fontStyle: "inherit", textAlign: "left" }}>
-                                    We chose quality above just doing it. Our solutions make sure you appreciate the beauty of great designs </p>
-                                </Col>
-                            </Row>
+                        <Col xs={11} sm={11} md={5} lg={5} className="col-background-hover" >
+                          <div className="icons-yellow"><EmojiNatureIcon /></div>
+                          <span style={{ fontSize: "28.9px", fontFamily: "Raleway", fontWeight: 400, lineHeight: "32px", opacity: "1" }}>Elegant Design</span>
+                          <p style={{ fontSize: "16px", lineHeight: "29px", opacity: "0.75", color: "#444", fontStyle: "inherit", textAlign: "left" }}>
+                            We understand the impact of beautiful designs and stunning works of art. We build with style using tools like figma</p>
                         </Col>
                     </Row>
-                </Container>
+                    <Row justify="space-between" style={{ paddingLeft: "-50" }} >
+                        <Col xs={11} sm={11} md={5} lg={5} className="col-background-hover" >
+                          <div className="icons-yellow"><SportsKabaddiIcon /></div>
+                          <span style={{ fontSize: "28.9px", fontFamily: "Raleway", fontWeight: 400, lineHeight: "32px", opacity: "1" }}>User Centric</span>
+                          <p style={{ fontSize: "16px", lineHeight: "29px", opacity: "0.75", color: "#444", fontStyle: "inherit", textAlign: "left" }}>
+                            At the center of our designs is the belief that the user is the definer of purpose. We thus create designs that improve human experience. </p>
+                        </Col>
+                        <Col xs={11} sm={11} md={5} lg={5} className="col-background-hover" >
+                          <div className="icons-blue"><GestureIcon /></div>
+                          <span style={{ fontSize: "28.9px", fontFamily: "Raleway", fontWeight: 400, lineHeight: "32px", opacity: "1" }}>Quality Tested</span>
+                          <p style={{ fontSize: "16px", lineHeight: "29px", opacity: "0.75", color: "#444", fontStyle: "inherit", textAlign: "left" }}>
+                            We chose quality above just doing it. Our solutions make sure you appreciate the beauty of great designs </p>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
+            </Container>
             </div>
-            <div >
-                <Row style={{ width: "200", paddingTop: "30px", paddingLeft: "20px", paddingRight: "20px", 
-                              paddingBottom: "30px", backfaceVisibility: "hidden", boxSizing: "border-box", 
-                              backgroundColor: "#f2f2f2" }} > 
-                    <div style={{ paddingLeft: "90px", paddingRight: "90px", justifyItems: "center" }} >
-                        <Card className={classes.root} style={{ boxShadow: "70", borderRadius: "20px", paddingLeft: "50px", paddingRight: "50px", paddingTop: "15px", paddingBottom: "15px" }} >
-                            <CardActionArea style={{ borderRadius: "20px", paddingTop: "20px", paddingBottom: "20px" }} > 
-                                <CardContent>
-                                    <Typography gutterBottom variant="h5" component="h2" style={{ textAlign: "center", 
-                                        fontFamily: "cursive", fontStyle: "normal", fontWeight: 700, color: "#ffffff",
-                                        fontSize: "35.2px", lineHeight: "41.6px" }} >
-                                    Build your dream website or app today.
-                                    </Typography>
-                                    <div style={{ paddingTop: "10px", paddingLeft: "73px", paddingRight: "73px "}} >
-                                        <Button variant="outlined" color="primary" style={{ justifyItems: "center" }} >
-                                            Request quote
-                                        </Button>
-                                    </div>
-                                </CardContent>
-                            </CardActionArea>
-                        </Card>
+            
+            <div style={{ backgroundColor: "#f2f2f2", width: "100%", 
+                    borderTopLeftRadius: "80px", borderTopRightRadius: "80px", backfaceVisibility: "visible" }} >
+                <Container >
+                <Row xs={12} sm={12} md={6} lg={12} > 
+                    <div className={ classes.root } style={{ width: "100%", height: "220px", borderRadius: "20px", paddingTop: "80px", paddingLeft: "10px", paddingRight: "10px" }}>
+                        <Typography gutterBottom variant="h5" component="h2" style={{ textAlign: "center", 
+                            fontFamily: "cursive", fontStyle: "normal", fontWeight: 700, color: "#ffffff",
+                            fontSize: "35.2px", lineHeight: "41.6px" }} >
+                            Build your dream website or app today.
+                        </Typography>
+                        <Row xs={12} sm={12} md={6} lg={12} style={{ paddingTop: "30px", position: "relative", margin: "auto", display: "flex" }} >
+                            <Button variant="outlined" style={{ color: "whitesmoke", borderColor: "whitesmoke", borderBlockWidth: "2px" }} >
+                                Request quote
+                            </Button>
+                        </Row>    
                     </div>
                 </Row>
+                </Container>
             </div>
         </>
     );
